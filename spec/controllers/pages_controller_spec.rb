2bf324fa07b02d14f4228eefb920e7bb904385d4
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2017-07-20 11:53:52 +0300

    Add rules to rspec controller Pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
 
-  describe 'home' do
-    it 'renders home page' do
-      get :home
-      expect(response).to render_template("home")
-      expect(response).to have_http_status(200)
-    end
+  it 'renders home page' do
+    get :home
+    expect(response).to render_template("home")
+    expect(response).to have_http_status(200)
   end
+
+
+
+  it 'renders rules page' do
+    get :rules
+    expect(response).to render_template("rules")
+    expect(response).to have_http_status(200)
+  end
+
 end
