fc514647f548c276572180d0e7d9df2e83fd7399
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2017-08-07 11:00:26 +0300

    Drop bootsy tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802123135) do
+ActiveRecord::Schema.define(version: 20170807075938) do
 
   create_table "announcements", force: :cascade do |t|
     t.string "title"
